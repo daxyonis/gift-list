6e6d925507e64872dff9999e4f90fb11ed6a87ab
daxyonis <emaciejko@gmail.com>
2021-04-15 14:27:13 -0400

    Disable test

--- src/test/java/com/evkosoft/portfoliocrud/PortfolioCrudApplicationTests.java
+++ src/test/java/com/evkosoft/portfoliocrud/PortfolioCrudApplicationTests.java
@@ -3,10 +3,10 @@ package com.evkosoft.portfoliocrud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class PortfolioCrudApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
